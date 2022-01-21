4609a7ef822e2acdd8590f30562db9157455b1e0
david.alves <david.alves@farfetch.com>
2022-01-21 14:57:43 -0300

    feat: add challenge 5

---
+++ src/challenge5.ts
@@ -0,0 +1,65 @@
+// Classes, herança e métodos de acesso aos atributos
+class Moto {
+  constructor(
+    private nome: string,
+    public velocidade: number = 0
+  ) {}
+
+  buzinar() {
+    console.log('Toooooooooot!');
+  }
+
+  acelerar(delta: number) {
+    this.velocidade = this.velocidade + delta;
+  }
+}
+
+const moto = new Moto('Ducati')
+
+moto.buzinar()
+console.log(moto.velocidade)
+
+moto.acelerar(30)
+console.log(moto.velocidade)
+
+// Exercício 2 - Herança
+abstract class Objeto2D {
+  constructor(
+    protected base: number,
+    protected altura: number,
+  ) {}
+
+  abstract area(): number
+}
+
+class Retangulo extends Objeto2D {
+  area() {
+    return this.base * this.altura;
+  }
+}
+
+const retangulo = new Retangulo(5, 7);
+console.log('Área retângulo 5x7:', retangulo.area());
+
+// Exercício 3 - Getters & Setters
+class Estagiario {
+  private _primeiroNome: string = '';
+
+  get primeiroNome() {
+    return this._primeiroNome
+  }
+
+  set primeiroNome(nome: string) {
+    this._primeiroNome = nome;
+  }
+}
+
+const estagiario = new Estagiario()
+
+console.log(estagiario.primeiroNome)
+
+estagiario.primeiroNome = 'Le'
+console.log(estagiario.primeiroNome)
+
+estagiario.primeiroNome = 'Leonardo'
+console.log(estagiario.primeiroNome)
